67f24253d9462b67aad5bb132b49f8b6d0388efc
DanielGreene101 <d.b.greene101@gmail.com>
2017-10-11 10:52:34 -0500

    finished

---
+++ dog_test.rb
@@ -0,0 +1,26 @@
+require 'minitest/autorun'
+require_relative 'animal'
+require_relative 'dogs'
+
+class DogTest < Minitest::Test 
+	def setup
+		@dog = Dog.new("Dog")
+	end
+
+	def test_initialize_with_no_params
+		assert_equal "Dog", @dog.name
+	end
+
+	def test_dog_walk_speed
+		@dog.speed = 55
+		assert_equal 55, @dog.speed
+	end
+	def test_legs_speed
+		@dog.legs = 4
+		current_speed = @dog.speed
+		@dog.walk
+		assert_equal (current_speed + (0.2 * 4)),
+		@dog.speed
+	end
+
+end
